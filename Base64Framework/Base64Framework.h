a14243f17df8efe815ee3acde6431562e4e28d8e
Jon Brooks <thomas.brooks@mheducation.com>
2016-09-30 14:47:21 -0700

    First try getting it to build under Carthage

---
+++ Base64Framework/Base64Framework.h
@@ -0,0 +1,22 @@
+//
+//  Base64Framework.h
+//  Base64Framework
+//
+//  Created by jbrooks on 9/30/16.
+//  Copyright © 2016 Freshcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base64Framework.
+FOUNDATION_EXPORT double Base64FrameworkVersionNumber;
+
+//! Project version string for Base64Framework.
+FOUNDATION_EXPORT const unsigned char Base64FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base64Framework/PublicHeader.h>
+#import <Base64Framework/MF_Base64Additions.h>
+
+
+
+
